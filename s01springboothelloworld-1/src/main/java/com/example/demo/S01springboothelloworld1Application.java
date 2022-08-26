bb07e026b696b013158b54f6c10350d0c8f0f03f
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2022-08-27 02:20:09 +0530

    changes

---
+++ s01springboothelloworld-1/src/main/java/com/example/demo/S01springboothelloworld1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S01springboothelloworld1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S01springboothelloworld1Application.class, args);
+	}
+
+}
